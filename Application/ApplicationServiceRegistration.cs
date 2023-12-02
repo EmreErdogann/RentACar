9f6e164414bb18a942c2d5f277d7db9bbbcb568f
emree <emre.erdogan@erstream.com>
2023-12-02 22:00:07 +0300

    Added Global Error Management

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Core.Application.Rules;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Application
@@ -9,6 +10,8 @@ namespace Application
         {
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
 
             services.AddMediatR(configuration =>
             {
---
+++ Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Brands.Constants;
+using Application.Services.Repositories;
+using Core.Application.Rules;
+using Core.CrossCuttingConcerns.Extensions.Types;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Rules;
+
+public class BrandBusinessRules : BaseBusinessRules
+{
+    private readonly IBrandRepository _brandRepository;
+    public BrandBusinessRules(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository; 
+    }
+
+    public async Task BrandNameCannotBeDuplicatedWhenInserted(string name)
+    {
+        Brand? brand = await _brandRepository.GetAsync(predicate: b => b.Name.ToLower()== name.ToLower());
+        if (brand != null)
+        {
+            throw new BusinessException(BrandsMessages.BrandNameExists);
+        }
+    }
+}
